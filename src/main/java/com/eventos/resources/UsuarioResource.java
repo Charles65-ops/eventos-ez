5986a5566e867e4baf2ee49b99dcf6fa445741a1
Charles65-ops <charlessasmgunlima68@gmail.com>
2025-02-20 19:45:16 -0300

    eventos ez

---
+++ src/main/java/com/eventos/resources/UsuarioResource.java
@@ -0,0 +1,26 @@
+package com.eventos.resources;
+
+import com.eventos.dtos.UsuarioDTO;
+import com.eventos.models.Usuario;
+import com.eventos.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping()
+    public String getUsuarios() {
+        return "testar retorno dos Usuarios";
+    }
+
+    @PostMapping()
+    public ResponseEntity<UsuarioDTO> criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+         return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTO));
+    }
+}
